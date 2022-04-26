540002de74321c357c944f5fb5ba829631504747
Everton Zardo <evezardo15@gmail.com>
2022-04-26 18:23:56 -0300

    Rock-Paper-Scissors project

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Rock-Paper-Scissors
+
+Rock-Paper-Scissors project from The Odin Project, using only the basics of JavaScript
--- script.js
+++ script.js
@@ -6,36 +6,72 @@ function computerPlay(){
 
 function playRound(playerSelection, computerSelection){
     let string;
+    let number;
 
     playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
 
     if (playerSelection === computerSelection){
         string = "Draw!";
+        number = 0;
     }
     else if (playerSelection === "Paper" && computerSelection === "Rock"){
-        string = "Player won! Paper beats rock!";
+        string = "User won! Paper beats rock!";
+        number = 1;
     }
     else if (playerSelection === "Scissors" && computerSelection === "Paper"){
-        string = "Player won! Scissors beats paper!";
+        string = "User won! Scissors beats paper!";
+        number = 1;
     }
     else if (playerSelection === "Rock" && computerSelection === "Scissors"){
-        string = "Player won! Rock beats scissors!";
+        string = "User won! Rock beats scissors!";
+        number = 1;
     }
 
     if (computerSelection === "Paper" && playerSelection === "Rock"){
         string = "Computer won! Paper beats rock!";
+        number = 2;
     }
     else if (computerSelection === "Scissors" && playerSelection === "Paper"){
         string = "Computer won! Scissors beats paper!";
+        number = 2;
     }
     else if (computerSelection === "Rock" && playerSelection === "Scissors"){
         string = "Computer won! Rock beats scissors!";
+        number = 2;
     }
 
-    return string;
+    console.log(string);
+
+    return number;
 }
 
+function game(){
+    let playerWon = 0;
+    let computerWon = 0;
+    let result;
 
-const playerSelection = "rock";
-const computerSelection = computerPlay();
-console.log(playRound(playerSelection, computerSelection));
+    for (let i = 0; i < 5; i ++){
+        const playerSelection = prompt("Choose between rock, paper and scissors: ", "Paper");
+        const computerSelection = computerPlay();
+
+        result = playRound(playerSelection, computerSelection);
+
+        if (result === 1){
+            playerWon++;
+        } else if (result === 2){
+            computerWon++;
+        } 
+    }
+
+    if (playerWon > computerWon){
+        console.log("The winner in 5 rounds is the User!");
+    }
+    else if (computerWon > playerWon){
+        console.log("The winner in 5 rounds is the Computer!");
+    }
+    else {
+        console.log("There's no winner!");
+    }
+}
+
+game();
