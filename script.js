5932613738d4966d3f600488cc17e233c04b3c95
Everton Zardo <evezardo15@gmail.com>
2022-05-13 12:34:45 -0300

    Refactored most of the code in script.js

--- script.js
+++ script.js
@@ -1,52 +1,87 @@
+let showRoundResult = document.querySelector('#round-result');
+let pComputer = document.querySelector('#computer-count');
+let pPlayer = document.querySelector('#player-count');
+let pDraw = document.getElementById('draw-count');
+let btn = document.querySelectorAll('.btn');
+let container = document.getElementById('result');
+let inFiveRounds = document.createElement('p'); // USED TO SHOW WHO'S THE WINNER AFTER 5 ROUNDS
+inFiveRounds.setAttribute('id', 'show-winner');
+
+let playerWins = 0;
+let computerWins = 0;
+let draw = 0;
+let roundCount = 0;
+
+// FUNCTION FOR THE COMPUTER CHOICE
 function computerPlay(){
     let items = ["Rock", "Paper", "Scissors"];
 
     return items[Math.floor(Math.random() * items.length)];
 }
 
+// FUNCTION THAT PLAYS THE ROUND BETWEEN THE USER AND THE COMPUTER
 function playRound(playerSelection, computerSelection){
-    let string;
-    let number;
+    let showRoundWinner;
+    let roundWinner;
 
     playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
 
     if (playerSelection === computerSelection){
-        string = "Draw!";
-        number = 0;
+        showRoundWinner = "Draw!";
+        roundWinner = 0;
     }
-    else if (playerSelection === "Paper" && computerSelection === "Rock"){
-        string = "User won! Paper beats rock!";
-        number = 1;
+    else if 
+    (
+        playerSelection === "Paper" && computerSelection === "Rock" || 
+        playerSelection === "Scissors" && computerSelection === "Paper" ||
+        playerSelection === "Rock" && computerSelection === "Scissors"
+    ){
+        showRoundWinner = "User won the round!";
+        roundWinner = 1;
     }
-    else if (playerSelection === "Scissors" && computerSelection === "Paper"){
-        string = "User won! Scissors beats paper!";
-        number = 1;
-    }
-    else if (playerSelection === "Rock" && computerSelection === "Scissors"){
-        string = "User won! Rock beats scissors!";
-        number = 1;
-    }
-
-    if (computerSelection === "Paper" && playerSelection === "Rock"){
-        string = "Computer won! Paper beats rock!";
-        number = 2;
-    }
-    else if (computerSelection === "Scissors" && playerSelection === "Paper"){
-        string = "Computer won! Scissors beats paper!";
-        number = 2;
-    }
-    else if (computerSelection === "Rock" && playerSelection === "Scissors"){
-        string = "Computer won! Rock beats scissors!";
-        number = 2;
+    else {
+        showRoundWinner = "Computer won the round!";
+        roundWinner = 2;
     }
 
-    return string;
+    roundCount++;
+    showRoundResult.textContent = showRoundWinner;
+    return roundWinner;
 }
 
-let btn = document.querySelectorAll('.btn');
-let showResult = document.querySelector('#result');
+function executeFunc(parameter){
+    let roundResult;
 
+    roundResult = playRound(parameter.textContent, computerPlay());
+    switch (roundResult) {
+        case 1:
+            playerWins++;
+            pPlayer.textContent = "Player rounds win count: " + playerWins;
+            break;
+        
+        case 2:
+            computerWins++;
+            pComputer.textContent = "Computer rounds win count: " + computerWins;
+            break;
+
+        default:
+            draw++;
+            pDraw.textContent = "Draw count: " + draw;
+            break;
+    }
+
+    if(playerWins == 5 && inFiveRounds.textContent.length == 0){
+        inFiveRounds.textContent = "The winner in 5 rounds is the Player!";
+        container.appendChild(inFiveRounds);
+    }
+
+    if(computerWins == 5 && inFiveRounds.textContent.length == 0){
+        inFiveRounds.textContent = "The winner in 5 rounds is the Computer!";
+        container.appendChild(inFiveRounds);
+    }
+}
+
+// WHEN ONE OF THE BUTTONS IS CLICKED THE executeFunc() IS TRIGGERED
 Array.from(btn).forEach(element => {
-    element.addEventListener('click', 
-    () => showResult.textContent = playRound(element.textContent, computerPlay()));
+    element.addEventListener('click', () => executeFunc(element));
 });
